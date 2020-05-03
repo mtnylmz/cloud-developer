076e127b3b76e757e5c91f301ad077c51a67fa75
METIN YILMAZ <TCMETYILMAZ@turkcell.entp.tgc>
2020-05-03 10:46:29 +0300

    Deneme 1 - no idea

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,13 +1,14 @@
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
+    "username": "postgres",
+    "password": "postgres",
+    "database": "myFirstDB",
+    "host": "myudagramdb.cxaldv5vwryy.us-east-1.rds.amazonaws.com",
+    "port":8080,
     "dialect": "postgres",
-    "aws_region": "us-east-2",
+    "aws_region": "us-east-1",
     "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+    "aws_media_bucket": "udagram-metin-dev"
   },
   "prod": {
     "username": "",
--- course-02/exercises/udacity-c2-restapi/src/sequelize.ts
+++ course-02/exercises/udacity-c2-restapi/src/sequelize.ts
@@ -1,7 +1,6 @@
 import {Sequelize} from 'sequelize-typescript';
 import { config } from './config/config';
 
-
 const c = config.dev;
 
 // Instantiate new Sequelize instance!
@@ -10,7 +9,7 @@ export const sequelize = new Sequelize({
   "password": c.password,
   "database": c.database,
   "host":     c.host,
-
+  "port":     c.port,
   dialect: 'postgres',
   storage: ':memory:',
 });
